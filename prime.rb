3ed13811075345a91e927e970e6834ad8e3c60f4
Laika Deunert <laikadeunert@Laikas-Laptop.local>
2021-01-17 11:36:12 -0800

    commit tree 1995978ce2f004ad699d5194c1b5f5140eee00aa

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+def prime?(num)
+
+    if num <= 1
+
+        return false 
+    end
+
+    (2..(num-1)).each do |n|
+        # starts at 2 to (5 -1) and checks if 5 is
+        # divisable by any number in between 2 & 4
+        if num % n == 0 
+            return false 
+        end
+    end
+    true
+end
